a898c44f8dc622d91812abc60d7fa7977a2177a5
YERIM <dnaer456@gmail.com>
2020-12-13 13:39:26 +0900

    server

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
 import express from "express";
-import mogran from "morgan";
+import morgan from "morgan";
 import dotenv from "dotenv";
 dotenv.config();
 import path from "path";
+import mongoose from "mongoose";
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.set("view engine", "pug");
+
+app.use(morgan(`dev`));
+
+app.listen(PORT, () => {
+  console.log(`✅  ${PORT} Server Start`);
+});
