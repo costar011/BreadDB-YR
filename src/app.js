c34fc3ac2cc7ebb6d110e4b089ecbc099eaddad2
YERIM <dnaer456@gmail.com>
2020-12-14 22:20:41 +0900

    ee

--- src/app.js
+++ src/app.js
@@ -17,7 +17,37 @@ app.use(morgan(`dev`));
 
 app.use(express.static(path.join(__dirname, "/assets")));
 
+mongoose.connect(
+  `mongodb://4leaf:fourleaf0309@210.114.1.127:27017/admin`,
+  {
+    dbName: `JYR_PRA`,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  },
+  (error) => {
+    if (error) {
+      console.log("❌ Failed");
+    } else {
+      console.log("⭕️  DB  ");
+    }
+  }
+);
+
 app.get("/", globalRouter);
+
+app.get("/bread", globalRouter);
+
+app.get("/", async (req, res) => {
+  const result = await Bread.find({}, {});
+
+  return res.render("screens/home", { bread: result });
+});
+
+app.get("/bread", async (req, res) => {
+  const result = await Bread.find({}, {});
+
+  return res.render("screens/bread", { bread: result });
+});
 
 app.listen(PORT, () => {
   console.log(`✅  ${PORT} Server Start`);
